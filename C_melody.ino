9169104f4a1ff969fc0864edbb1f4e67891ef102
kaira <kairazhangshan50@gmail.com>
2026-06-30 11:58:40 +0900

    更新（06/30）

--- C_melody.ino
+++ C_melody.ino
@@ -1,94 +1,121 @@
-#include<Arduino.h>
-
-  int noteIndex = 0; //現在の音符ポインタ
-  unsigned long t_next = 0; //次の拍の予測絶対時間
-  uint8_t sendTime = 0; //シリアル通信を行う時間
-  bool beatUpdated = false; //拍予測の更新
-  enum State {IDLE , PREBEAT , PLAYING , TEMPO_CHANGE , FERMATA };
-  State state =IDLE; //現在の状態
-  uint8_t note_off = 1; //前の音の終了
-  uint8_t note = 0; //音名インデックス（休符＝２５５）
-  uint8_t vel = 0; //音の強さ（0~127）
-  const uint8_t score[][2] ={
-    {0, 100},//ド　
-    {1, 100},//レ
-    {2, 100},//ミ
-    {3, 100},//ファ｜
-    {2, 100},//ミ　
-    {1, 100},//レ
-    {0, 100},//ド｜
-    {2, 127},//ミ
-    {3, 127},//ファ
-    {4, 127},//ソ
-    {5, 127},//ラ｜
-    {4, 127},//ソ
-    {3, 127},//ファ
-    {2, 127},//ミ｜
-    {0, 90},//ド
-    {0, 90},//ド｜
-    {0, 90},//ド
-    {0, 90},//ド｜
-    {0, 100},//ド
-    {0, 100},//ド
-    {1, 100},//レ
-    {1, 100},//レ
-    {2, 100},//ミ
-    {2, 100},//ミ
-    {3, 100},//ファ
-    {3, 100},//ファ｜
-    {2, 100},//ミ
-    {1, 100},//レ
-    {0, 100}//ド
+int noteIndex = 0; //現在の音符ポインタ
+unsigned long soundTime;
+unsigned long sendTime = 0; //シリアル通信を行う時間
+uint8_t note_off = 1; //前の音の終了
+uint8_t note = 0; //音名インデックス（休符＝7）
+uint8_t vel = 0; //音の強さ（0~127）
+uint8_t note_long = 0; //音の長さ
+bool sounded = true; //音を鳴らしたか
+const uint8_t score[][3] = {
+    {0, 100, 4},//ド　
+    {1, 100, 4},//レ
+    {2, 100, 4},//ミ
+    {3, 100, 4},//ファ｜
+    {2, 100, 4},//ミ　
+    {1, 100, 4},//レ
+    {0, 100, 2},//ド｜
+    {2, 127, 4},//ミ
+    {3, 127, 4},//ファ
+    {4, 127, 4},//ソ
+    {5, 127, 4},//ラ｜
+    {4, 127, 4},//ソ
+    {3, 127, 4},//ファ
+    {2, 127, 2},//ミ｜
+    {0, 90,  4},//ド
+    {6, 100, 4},
+    {0, 90,  4},//ド｜
+    {6, 100, 4},
+    {0, 90,  4},//ド
+    {6, 100, 4},
+    {0, 90,  4},//ド｜
+    {6, 100, 4},
+    {0, 100, 8},//ド
+    {0, 100, 8},//ド
+    {1, 100, 8},//レ
+    {1, 100, 8},//レ
+    {2, 100, 8},//ミ
+    {2, 100, 8},//ミ
+    {3, 100, 8},//ファ
+    {3, 100, 8},//ファ｜
+    {2, 100, 4},//ミ
+    {1, 100, 4},//レ
+    {0, 100, 4},//ド
+    {6, 100, 4}
   };
-  const int SCORE_LEN = sizeof ( score ) / sizeof ( score[0]);
+const int SCORE_LEN = sizeof ( score ) / sizeof ( score[0]);
 
-  unsigned long sendTime_set(unsigned long t_next) {//担当Bよりより出された次の拍の予測絶対時間に補正値をかける
-  unsigned long sendTime = t_next;
-
+unsigned long sendTime_set(unsigned long nextBeatTime) {//担当Bよりより出された次の拍の予測絶対時間に補正値をかける
+  unsigned long sendTime = nextBeatTime;
   return sendTime;
-
 }
 
 
-void setup() {
-  Serial.begin(115200);
+//void setup() {
+ // Serial.begin(115200);
 
   //Serial.print("Note_OFF");
   //Serial.print(",");
   //Serial.print("NOTE");
   //Serial.print(",");
   //Serial.println("VEL");
-}
+//}
 
 
-void loop() {
-  if (state != FERMATA) {
-    if(noteIndex <= SCORE_LEN){
-
-      if (beatUpdated == true) {
-        sendTime = sendTime_set(t_next);
-
-        note = score[noteIndex][0];
-        vel  = score[noteIndex][1];
-        
-         if (millis() >= sendTime){
-           Serial.write(note_off);
-           Serial.write(note);
-           Serial.write(vel);
-
-           //Serial.print(note_off);
-           //Serial.print(",");
-           //Serial.print(note);
-           //Serial.print(",");
-           //Serial.println(vel);
-
-           noteIndex++;
-           beatUpdated = false;
-
+void C_loop() {
+  if (state != IDLE && state != CALIBRATE && state != PREBEAT && state != FERMATA) {
+    if(noteIndex < SCORE_LEN){
+      note = score[noteIndex][0];
+      vel  = score[noteIndex][1];
+      note_long = score[noteIndex][2];
+      if (beatUpdated && sounded) {
+        sendTime = sendTime_set(nextBeatTime);
+        if (note_long == 8 && i == 1) {
+          sendTime = currentBeatTime + ( sendTime - currentBeatTime ) * 0.5;
+        };
+        sounded = false;
+        beatUpdated = false;
+      };
+      if (note_long == 8 && millis() >= sendTime && !sounded) {
+        Serial.write(note_off);
+        Serial.write(note);
+        Serial.write(vel);
+        Serial.write(note_long);
+        noteIndex++;
+        sounded = true;
+        if (i == 0) {
+          i++;
         }
-      
-      }
-    }
-  }
- 
-}
+        else if (i == 1) {
+          beatUpdated = true;
+          i = 0;
+        };
+      };
+      if (note_long == 4 && millis() >= sendTime && !sounded){
+        Serial.write(note_off);
+        Serial.write(note);
+        Serial.write(vel);
+        Serial.write(note_long);
+        noteIndex++;
+        sounded = true;
+      };
+      if (note_long == 2 && millis() >= sendTime && !sounded){
+        if (i==0){
+          Serial.write(note_off);
+          Serial.write(note);
+          Serial.write(vel);
+          Serial.write(note_long);
+          sounded = true;
+          i++;          
+        }
+        else if (i == 1) {
+          sounded = true;
+          noteIndex++;
+          i = 0;
+        };
+      };
+      if (noteIndex == SCORE_LEN) noteIndex = 0;
+    };
+  };
+};
+
+
